1fa5307cb19cfa5a96da9438838dab1e2e8e2cc5
Josephine Nana Yaa Asare <nanayaaasare18@gmail.com>
2022-09-22 14:20:50 -0700

    initial commit

---
+++ fetchData.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
+exports.fetchData = async (endPoint) => {
+  try {
+    const results = await axios(`${baseUrl}/${endPoint}`);
+    return results.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
